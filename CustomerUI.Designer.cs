df2f56e6f4ec177643c3b9fd71823f2a00e9f1a2
sxj54480 <sxj54480@ucmo.edu>
2017-01-21 19:31:26 -0500

    Add files via upload

---
+++ CustomerUI.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace CustomerProject_Lab4_
+{
+    partial class CustomerUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.lblBillAmount = new System.Windows.Forms.Label();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.txtBillAmount = new System.Windows.Forms.TextBox();
+            this.txtProduct = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dtgCustomers = new System.Windows.Forms.DataGridView();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(7, 35);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(79, 13);
+            this.lblCustomerName.TabIndex = 0;
+            this.lblCustomerName.Text = "Custome Name";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(92, 32);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(96, 20);
+            this.txtCustomerName.TabIndex = 1;
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Location = new System.Drawing.Point(4, 69);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(75, 13);
+            this.lblProductName.TabIndex = 2;
+            this.lblProductName.Text = "Product Name";
+            // 
+            // lblBillAmount
+            // 
+            this.lblBillAmount.AutoSize = true;
+            this.lblBillAmount.Location = new System.Drawing.Point(212, 70);
+            this.lblBillAmount.Name = "lblBillAmount";
+            this.lblBillAmount.Size = new System.Drawing.Size(59, 13);
+            this.lblBillAmount.TabIndex = 3;
+            this.lblBillAmount.Text = "Bill Amount";
+            this.lblBillAmount.Click += new System.EventHandler(this.lblBillAmount_Click);
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(211, 39);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(78, 13);
+            this.lblPhoneNumber.TabIndex = 4;
+            this.lblPhoneNumber.Text = "Phone Number";
+            // 
+            // txtBillAmount
+            // 
+            this.txtBillAmount.Location = new System.Drawing.Point(295, 63);
+            this.txtBillAmount.Name = "txtBillAmount";
+            this.txtBillAmount.Size = new System.Drawing.Size(95, 20);
+            this.txtBillAmount.TabIndex = 6;
+            this.txtBillAmount.Text = "0";
+            // 
+            // txtProduct
+            // 
+            this.txtProduct.Location = new System.Drawing.Point(92, 66);
+            this.txtProduct.Name = "txtProduct";
+            this.txtProduct.Size = new System.Drawing.Size(96, 20);
+            this.txtProduct.TabIndex = 7;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(295, 35);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(95, 20);
+            this.txtPhoneNumber.TabIndex = 8;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(92, 89);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Save";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dtgCustomers
+            // 
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtgCustomers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dtgCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dtgCustomers.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dtgCustomers.Location = new System.Drawing.Point(-5, 117);
+            this.dtgCustomers.Name = "dtgCustomers";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtgCustomers.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dtgCustomers.Size = new System.Drawing.Size(437, 150);
+            this.dtgCustomers.TabIndex = 10;
+            this.dtgCustomers.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgCustomers_CellContentClick);
+            this.dtgCustomers.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgCustomers_RowEnter);
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(92, 6);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(96, 20);
+            this.txtId.TabIndex = 11;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(4, 9);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(63, 13);
+            this.lblId.TabIndex = 12;
+            this.lblId.Text = "Customer Id";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(186, 89);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(295, 89);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // CustomerUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(507, 330);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.dtgCustomers);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.txtProduct);
+            this.Controls.Add(this.txtBillAmount);
+            this.Controls.Add(this.lblPhoneNumber);
+            this.Controls.Add(this.lblBillAmount);
+            this.Controls.Add(this.lblProductName);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.lblCustomerName);
+            this.Name = "CustomerUI";
+            this.Text = "Customer Management Application";
+            this.Load += new System.EventHandler(this.CustomerUI_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCustomers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.Label lblProductName;
+        private System.Windows.Forms.Label lblBillAmount;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.TextBox txtBillAmount;
+        private System.Windows.Forms.TextBox txtProduct;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dtgCustomers;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ CustomerUI.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogicCustomer;
+using Dal;
+using System.Configuration;
+
+namespace CustomerProject_Lab4_
+{
+    public partial class CustomerUI : Form
+    {
+        public CustomerUI()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadGrid()
+        {
+            CustomerDal dal = new CustomerDal();
+            DataSet customers = dal.Read();
+            dtgCustomers.DataSource = customers.Tables[0];
+
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Customer custobj = new Customer();
+             
+                custobj.CustomerName = txtCustomerName.Text;
+                Console.WriteLine(custobj.CustomerName);
+                custobj.PhoneNumber = txtPhoneNumber.Text;
+                Console.WriteLine(custobj.PhoneNumber);
+                custobj.ProductName = txtProduct.Text;
+                Console.WriteLine(custobj.ProductName);
+                custobj.BillAmount = Convert.ToDecimal(txtBillAmount.Text);
+                Console.WriteLine(custobj.BillAmount);
+             
+                
+                
+                if (custobj.Validate())//thrown
+                    {
+                    CustomerDal dal1 = new CustomerDal();
+                    dal1.Add(custobj);
+                    }
+            }
+            catch (Exception ex)
+            {
+              MessageBox.Show(ex.Message)  ;
+
+            }
+
+        }
+
+        private void lblBillAmount_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void CustomerUI_Load(object sender, EventArgs e)
+        {
+            //configuration xml is applied to the screen
+            this.Text = ConfigurationManager.AppSettings["NameoftheApplication"].ToString();
+            btnAdd.Text = ConfigurationManager.AppSettings["AddButton"].ToString();
+            btnDelete.Text = ConfigurationManager.AppSettings["UpdateButton"].ToString();
+            btnDelete.Text = ConfigurationManager.AppSettings["DeleteButton"].ToString();
+            LoadGrid();
+        }
+
+
+
+        private void dtgCustomers_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+
+            int rowselected = e.RowIndex;//current row
+            txtId.Text = dtgCustomers.Rows[rowselected].Cells[0].Value.ToString();
+            txtCustomerName.Text = dtgCustomers.Rows[rowselected].Cells[1].Value.ToString();
+            txtPhoneNumber.Text = dtgCustomers.Rows[rowselected].Cells[2].Value.ToString();
+            txtProduct.Text = dtgCustomers.Rows[rowselected].Cells[3].Value.ToString();
+           txtBillAmount.Text = dtgCustomers.Rows[rowselected].Cells[4].Value.ToString();
+
+        }
+
+        private void dtgCustomers_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //Loading  values from the ui
+            Customer updatedCustomer = new Customer();
+
+            updatedCustomer.CustomerName = txtCustomerName.Text;
+            updatedCustomer.PhoneNumber = txtPhoneNumber.Text;
+            updatedCustomer.ProductName = txtProduct.Text;
+            updatedCustomer.BillAmount = Convert.ToDecimal(txtBillAmount.Text);
+
+            //updating from the server
+            CustomerDal dal = new CustomerDal();
+            dal.Update(updatedCustomer, Convert.ToInt16(txtId.Text));
+           
+            //refreshing the grid
+            LoadGrid();
+            ClearUI();
+
+        }
+        private void ClearUI()
+        {
+
+            txtCustomerName.Text="";
+            txtPhoneNumber.Text="";
+            txtProduct.Text="";
+            txtBillAmount.Text="";
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            //Loading values from the ui
+            Customer DeletedCustomer = new Customer();
+
+            DeletedCustomer.CustomerName = txtCustomerName.Text;
+            DeletedCustomer.PhoneNumber = txtPhoneNumber.Text;
+            DeletedCustomer.ProductName = txtProduct.Text;
+            DeletedCustomer.BillAmount = Convert.ToDecimal(txtBillAmount.Text);
+
+            //updating from the server
+            CustomerDal dal = new CustomerDal();
+            dal.Delete(DeletedCustomer, Convert.ToInt16(txtId.Text));
+
+            //refreshing the grid
+            LoadGrid();
+            ClearUI();
+
+        }
+    }
+}
